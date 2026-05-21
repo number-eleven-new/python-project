83a6cc813c52d2b44d80a7bc2df487d03632c630
TootToot5 <ashermcody@gmail.com>
2026-05-20 23:37:55 -0700

    manually updated project files

--- app.py
+++ app.py
@@ -1,2 +1,4 @@
-print("Portfolio Project Launched")
-print("3")
+sequence_1 = list(input("Enter a DNA sequence: ")
+sequence_2 = list(input("Enter a DNA sequence: ")
+sequence_3 = list(input("Enter a DNA sequence: ")
+sequence_4 = list(input("Enter a DNA sequence: ")
