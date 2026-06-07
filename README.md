ac0c3168db9f6369a549fb68c121f87108bc324a
TootToot5 <ashermcody@gmail.com>
2026-06-06 21:37:41 -0700

    Added function that determines how much cytosine is in a DNA sequence

---
+++ README.md
@@ -0,0 +1,1 @@
+Project Successfully Started 
--- app.py
+++ app.py
@@ -1,4 +1,14 @@
-sequence_1 = list(input("Enter a DNA sequence: ")
-sequence_2 = list(input("Enter a DNA sequence: ")
-sequence_3 = list(input("Enter a DNA sequence: ")
-sequence_4 = list(input("Enter a DNA sequence: ")
+sequence_1 = list(input("Enter a DNA sequence: "))
+sequence_2 = list(input("Enter a DNA sequence: "))
+sequence_3 = list(input("Enter a DNA sequence: "))
+sequence_4 = list(input("Enter a DNA sequence: "))
+
+def add(sequence, base):
+	_ = 0
+	for u in sequence:
+		if u == base:
+		_ += 1
+	return _
+
+nucleotide = "C"
+result = add(sequence_1, nucleotide)
